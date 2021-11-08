77350506e429702a063413a4c8c712451f378ae7
Diogo Marques <dmarques2@gmail.com>
2021-11-08 18:52:32 -0300

    some postgres progress

--- packages/install-apps.sh
+++ packages/install-apps.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-sudo pacman -Syu --needed - < pkg.list
+pacman -Syu --needed - < pkg.list
 
 paru -Syu --needed - < aur.list
+
+groupadd docker
+
+usermod -aG docker $USER
+
+echo "127.0.0.1 desktop.cepel.br" >> /etc/hosts
+echo "127.0.0.1 $hostname.cepel.br" >> /etc/hosts
+
+#add 
+
+echo "listen_addresses = '*'" >> /var/lib/posgres/data/postgres.conf
+echo "host all all 172.17. 0.0/16 trust" >> /var/lib/postgres/data/pg_hba.conf
+
+docker pull dpage/pgadmin4
