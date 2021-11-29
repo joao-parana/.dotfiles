51202867b4bed1338b3653458323861d5149487d
Diogo Marques <dmarques2@gmail.com>
2021-11-29 10:45:46 -0300

    Typo fix

--- packages/install-apps.sh
+++ packages/install-apps.sh
@@ -17,13 +17,17 @@ echo "listen_addresses = '*'" >> /var/lib/posgres/data/postgres.conf
 echo "host all all 172.17. 0.0/16 trust" >> /var/lib/postgres/data/pg_hba.conf
 
 
-mkdir -p ~/.docker-volulmes/pgAdmin
-mkdir -p ~/.docker-volulmes/postgresql
+mkdir -p ~/.docker-volumes/pgAdmin
+mkdir -p ~/.docker-volumes/postgresql
 
-chattr +C ~/.docker-volulmes/postgresql
+chattr +C ~/.docker-volumes/postgresql
 
 
-sudo chown -R 5050:5050 ~/.docker-volulmes/pgAdmin
+sudo chown -R 5050:5050 ~/.docker-volumes/pgAdmin
 
+
+
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.54/bin/apache-tomcat-9.0.54.tar.gz
+tar -xf apache-tomcat-9.0.54.tar.gz
 
 docker pull dpage/pgadmin4
