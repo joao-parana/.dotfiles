b90dde86b936d185b5dbf6a85b7f5ebf4af3a114
Diogo Marques <dmarques2@gmail.com>
2021-12-02 13:24:52 -0300

    vscode review

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-code --install-extension "$(cat code-extensions.list | tr "\n" " ")"
+for a in $(< code-extensions.list tr "\n" " ")
+do
+    echo "Installing extension $a..."
+    code --install-extension "$a"
+done
